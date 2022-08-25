cf59a606c66e69d3a4aa63fb7face928f5612246
Belle <103557809+yxmauw@users.noreply.github.com>
2022-08-25 12:06:35 +0800

    Update model_methods.py

--- model_methods.py
+++ model_methods.py
@@ -8,14 +8,14 @@ import pickle
     
 def predict(new_data):
     # impute missing `Overall Qual` values
-    url = 'https://raw.githubusercontent.com/yxmauw/General_Assembly_Pub/main/project_2/cloud_app/streamlit_imp_data.csv'
+    url = 'https://github.com/yxmauw/houseprice-recommendation-heroku/blob/main/streamlit_imp_data.csv'
     imp_data = pd.read_csv(url, header=0)
     imp = KNNImputer()
     imp.fit(imp_data)
     shaped_data = np.reshape(new_data, (1, -1))
     input_data = imp.transform(shaped_data)
     # load model
-    with open('project_2/cloud_app/final_model.sav','rb') as f:
+    with open('final_model.sav','rb') as f:
         model = pickle.load(f)
     pred = model.predict([input_data][0])
     return pred 
